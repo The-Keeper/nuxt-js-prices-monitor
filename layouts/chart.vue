f1671b6a46e71e45d572f0db896feb74b4b8a45a
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2023-12-03 18:32:09 +0500

    simple chart

--- layouts/chart.vue
+++ layouts/chart.vue
@@ -1,55 +1,43 @@
 <template>
-    <highstock :options="chartOptions" />
-  </template>
-  
-  <script>
-  export default {
-    data () {
+  <highstock :options="chartOpts" />
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      series: []
+    }
+  },
+  computed: {
+    chartOpts () {
       return {
-        symbol: 'AAPL',
-        watchers: ['options.series'],
-        series: []
-      }
-    },
-    computed: {
-      chartOptions () {
-        return {
-          chart: {
-            type: 'candlestick'
-          },
-          rangeSelector: {
-            selected: 4
-          },
-          title: {
-            text: `${this.symbol} Stock Price`
-          },
-          series: this.series
-        }
-      }
-    },
-    mounted () {
-      this.fetchData('AAPL')
-    },
-    methods: {
-      async fetchData (symbol) {
-        const { price } = await fetch('/ohlcv.json').then(r => r.json())
-        this.series.push({
-          id: symbol.toLowerCase(),
-          name: symbol,
-          data: price.map((entry) => {
-            return [
-              entry.epochTime.epochTime,
-              entry.open.val,
-              entry.high.val,
-              entry.low.val,
-              entry.close.val
-            ]
-          })
-        }, { // Example with EMA
-          type: 'ema',
-          linkedTo: symbol.toLowerCase()
-        })
+        rangeSelector: {
+          selected: 4
+        },
+
+
+        tooltip: {
+          valueDecimals: 2,
+          split: true
+        },
+
+        series: this.series
       }
     }
+  },
+  mounted () {
+    const symbols = ['BTC']
+    symbols.forEach(s => this.fetchData(s))
+  },
+  methods: {
+    async fetchData (symbol) {
+      const data = await fetch(`/api/prices`).then(r => r.json())
+      this.series.push({
+        name: symbol,
+        data
+      })
+    }
   }
-  </script>
+}
+</script>
